c3ae93e48bbe0fe597afaa211b404242dbeb43e9
FrankFang <frankfang1990@gmail.com>
2024-03-14 02:43:07 +0800

    解析 sse message

--- src/initializers/init_sse.ts
+++ src/initializers/init_sse.ts
@@ -2,21 +2,35 @@ import { BotContext } from '@/bot'
 import EventSource from 'eventsource'
 import { Bot } from 'grammy'
 
-type Event = { data: { id: number; to: number; type: string; message: string } }
+type Event = {
+    type: string
+    lastEventId: string
+    origin: string
+    // data: { id: number; to: number; type: string; message: string }
+    data: string
+}
 
 let eventSource: EventSource
 export const initSse = (bot: Bot<BotContext>) => {
     const botId = getBotId()
     const url = process.env.API_BASE_URL + `/subscription/notification/customer/${botId}` // 替换为你的 SSE 服务器地址
     eventSource = new EventSource(url)
+    console.log('eventSource created')
     eventSource.onmessage = function (event: Event) {
-        switch (event.data.type) {
+        const data = JSON.parse(event.data)
+        switch (data.type) {
             case 'half_send':
             case 'all_send':
             case 'invite_link':
             case 'plan_expired_24h':
             case 'plan_expired':
-                bot.api.sendMessage(event.data.to, event.data.message)
+                bot.api.sendMessage(data.to, data.message)
+                break
+            case 'heartbeat':
+                console.log('heartbeat')
+                break
+            default:
+                console.log(event)
                 break
         }
     }
