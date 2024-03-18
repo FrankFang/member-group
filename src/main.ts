b31157e008206597756a1653ff2f5221c3fb584c
ff <ff_online@outlook.com>
2024-03-17 17:13:59 -0700

    handle unhandled rejection

--- src/main.ts
+++ src/main.ts
@@ -6,3 +6,7 @@ console.log('Bot is running')
 handle?.task?.()?.then(() => {
     console.log('Bot done processing!')
 })
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason)
+})
